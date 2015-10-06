c45af7fc2e6bd00da1462923710b3b17e230492f
mattmills49 <mattmills49@gmail.com>
2015-10-06 11:31:40 -0400

    chaned epa_model name in function

--- R/add_epa.R
+++ R/add_epa.R
@@ -14,12 +14,12 @@
 
 add_epa <- function(all_info, epa_model){
   run_plays <- dplyr::filter(all_info, Play.Type == "RUSH") %>% dplyr::mutate(Yards = ifelse(Yards > Spot, Spot, Yards))
-  run_plays$EPA <- expected_points[run_plays$Spot - run_plays$Yards + 1] - expected_points[run_plays$Spot + 1]
-  run_plays$EPA[run_plays$Turnover == "Fumble"] <- 0 - expected_points[run_plays$Spot[run_plays$Turnover == "Fumble"] + 1] / 2
+  run_plays$EPA <- epa_model[run_plays$Spot - run_plays$Yards + 1] - epa_model[run_plays$Spot + 1]
+  run_plays$EPA[run_plays$Turnover == "Fumble"] <- 0 - epa_model[run_plays$Spot[run_plays$Turnover == "Fumble"] + 1] / 2
 
   pass_plays <- dplyr::filter(all_info, Play.Type == "PASS") %>% dplyr::mutate(Yards = ifelse(Yards > Spot, Spot, Yards))
-  pass_plays$EPA <- expected_points[pass_plays$Spot - pass_plays$Yards + 1] - expected_points[pass_plays$Spot + 1]
-  pass_plays$EPA[pass_plays$Turnover %in% c("INT", "Fumble")] <- 0 - expected_points[pass_plays$Spot[pass_plays$Turnover %in% c("INT", "Fumble")] + 1]
+  pass_plays$EPA <- epa_model[pass_plays$Spot - pass_plays$Yards + 1] - epa_model[pass_plays$Spot + 1]
+  pass_plays$EPA[pass_plays$Turnover %in% c("INT", "Fumble")] <- 0 - epa_model[pass_plays$Spot[pass_plays$Turnover %in% c("INT", "Fumble")] + 1]
   return(list(run_info = run_plays, pass_info = pass_plays))
 }
   
